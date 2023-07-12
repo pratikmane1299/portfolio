2ecfff9bcc69717f81857e09eb02fb53b8b30063
Pratik Mane <pratik.mane1299@gmail.com>
2023-07-13 00:02:45 +0530

    fix: too many prisma client instances error.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,12 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
