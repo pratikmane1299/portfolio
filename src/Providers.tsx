12fee05a56fd00c25b89a23bcc6b530c59c5e4af
Pratik Mane <pratik.mane1299@gmail.com>
2024-03-04 21:56:25 +0530

    show progress bar during page transitions

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const queryClient = new QueryClient();
 
@@ -8,6 +9,14 @@ type ProvidersPropsType = PropsWithChildren<{}>;
 
 export default function Providers({ children }: ProvidersPropsType) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ProgressBar
+        height="3px"
+        color="#fd55b6"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </QueryClientProvider>
   );
 }
